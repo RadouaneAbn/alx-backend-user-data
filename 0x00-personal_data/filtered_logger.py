41ed773796f9f539e0a8b0b452e24cd7208f5df4
RadouaneAbn <radouaneabn@gmail.com>
2024-07-31 21:35:33 +0100

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,6 @@
 from typing import List
 import re
 import logging
-from datetime import datetime
 
 
 class RedactingFormatter(logging.Formatter):
@@ -15,11 +14,13 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List):
+    def __init__(self, fields: List[str]):
+        """ Init method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Format method """
         return filter_datum(
                 self.fields,
                 self.REDACTION,
