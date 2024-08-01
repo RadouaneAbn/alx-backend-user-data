13ba021e3b4b6f139ade8e7775389429146a9f5f
RadouaneAbn <radouaneabn@gmail.com>
2024-08-01 21:18:07 +0100

    Task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'),
                                     bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ This function checkes wheter a password is correct """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
