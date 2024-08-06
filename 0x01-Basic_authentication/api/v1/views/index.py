cf023664a9bccc6a4b1568fbcd8a594d40f3f162
RadouaneAbn <radouaneabn@gmail.com>
2024-08-06 14:37:14 +0100

    Task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized", methods=['GET'], strict_slashes=False)
+def unauthorized_route() -> None:
+    """ This route raises unauthorized error
+    """
+    abort(401)
