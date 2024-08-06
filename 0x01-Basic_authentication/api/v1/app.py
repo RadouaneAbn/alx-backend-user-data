95ab9ece62a8f89e11f111ea97afa30998cad71a
RadouaneAbn <radouaneabn@gmail.com>
2024-08-06 18:13:02 +0100

    Task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.auth import Auth
 from typing import Tuple
 
 
@@ -14,47 +13,45 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
 auth_type = getenv("AUTH_TYPE")
 if auth_type == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
 @app.errorhandler(404)
-def not_found(error: Exception) -> Tuple[jsonify, int]:
+def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error: Exception) -> Tuple[jsonify, int]:
+def unauthorized(error) -> str:
     """Unauthorized error handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error: Exception) -> Tuple[jsonify, int]:
+def forbidden(error) -> str:
     """ Forbidden error handler """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request_check() -> None:
+def before_request_check():
     """ This function will be executed before every request """
-    if auth is None:
-        return
-    forbidden_list = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-    if not auth.require_auth(request.path, forbidden_list):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth is not None:
+        forbidden_list = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/"
+        ]
+        if auth.require_auth(request.path, forbidden_list):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
