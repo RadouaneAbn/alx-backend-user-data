fa5e5c4872ada03d669a4454d2949020e8e46e0d
RadouaneAbn <radouaneabn@gmail.com>
2024-08-06 18:59:35 +0100

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ basic_auth Module """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -16,3 +18,17 @@ class BasicAuth(Auth):
                 or not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+    ) -> str:
+        """ This method return a decoded value of a Base64 string """
+        if base64_authorization_header is None\
+                or type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header
+            ).decode("utf-8")
+        except binascii.Error:
+            return None
