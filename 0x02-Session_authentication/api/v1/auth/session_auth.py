df14bd28899eddaa251f44e8cf718620d0a42103
RadouaneAbn <radouaneabn@gmail.com>
2024-08-08 16:50:59 +0100

    Task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" session_auth Module
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAth class for session authentication
+    """
+    pass
