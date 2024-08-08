ecc2a781e64fcaacca4fb010784ea47bc9d94e77
RadouaneAbn <radouaneabn@gmail.com>
2024-08-09 00:51:37 +0100

    Task 10

--- 0x02-Session_authentication/models/base.py
+++ 0x02-Session_authentication/models/base.py
@@ -28,7 +28,7 @@ class Base():
             self.created_at = datetime.strptime(kwargs.get('created_at'),
                                                 TIMESTAMP_FORMAT)
         else:
-            self.created_at = datetime.now()
+            self.created_at = datetime.utcnow()
         if kwargs.get('updated_at') is not None:
             self.updated_at = datetime.strptime(kwargs.get('updated_at'),
                                                 TIMESTAMP_FORMAT)
@@ -126,6 +126,7 @@ class Base():
         """ Search all objects with matching attributes
         """
         s_class = cls.__name__
+
         def _search(obj):
             if len(attributes) == 0:
                 return True
@@ -133,5 +134,5 @@ class Base():
                 if (getattr(obj, k) != v):
                     return False
             return True
-        
+
         return list(filter(_search, DATA[s_class].values()))
