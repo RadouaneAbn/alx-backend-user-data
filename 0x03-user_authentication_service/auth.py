de49871faac7bc29eaed1950848232016db4fe92
RadouaneAbn <radouaneabn@gmail.com>
2024-08-16 00:43:01 +0100

    Task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,11 +50,10 @@ class Auth:
         return False
 
     def create_session(self, email: str) -> str:
-        """ This method create a session for a user if found """
+        """ This method create a session for a user if  """
         try:
             user = self._db.find_user_by(email=email)
             user.session_id = _generate_uuid()
-            self._db._session.commit()
             return user.session_id
         except NoResultFound:
             pass
