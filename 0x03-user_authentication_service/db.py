dbdce3c419fc073c76c9677c19b02915b363b9a1
RadouaneAbn <radouaneabn@gmail.com>
2024-08-15 20:20:05 +0100

    Task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,7 +49,7 @@ class DB:
             raise NoResultFound()
         return user
 
-    def update_user(self, user_id: int, **kwargs: Dict) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """ This method updates a user """
         user = self.find_user_by(id=user_id)
         if not all(hasattr(User, key) for key in kwargs.keys()):
