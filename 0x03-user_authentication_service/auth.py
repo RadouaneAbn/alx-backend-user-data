92ff9aaab59e2ede2df76ef1dc5742462c1f42c8
RadouaneAbn <radouaneabn@gmail.com>
2024-08-16 01:05:38 +0100

    Task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,12 @@ class Auth:
             return user.session_id
         except NoResultFound:
             pass
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ This method find a user by its session_id and return """
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
